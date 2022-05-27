94e77cf009762c8571d6bb3ea936e5ea1c419e68
Fabricio Manrique <fabemanrique@hotmail.com>
2022-05-27 17:28:46 -0300

    Initial commit

---
+++ Source/ShooterPrototype/ShooterPrototypeGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShooterPrototypeGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AShooterPrototypeGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AShooterPrototypeGameMode();
+};
+
+
+
